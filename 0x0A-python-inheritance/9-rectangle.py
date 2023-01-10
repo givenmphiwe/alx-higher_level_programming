c6b065c8037708c63b5e43f2fe9248989068f740
givenmphiwe <giggslive52@gmail.com>
2023-01-09 22:47:34 -0800

    9task

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -13,8 +13,8 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """Method for initialized the attrubutes"""
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
 
     def area(self):
