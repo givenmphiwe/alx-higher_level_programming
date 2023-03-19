3b7047a029c931a6fec31564ef2cb8bf2c3c2701
Given Makhubela <giggslive52@gmail.com>
2023-03-19 23:48:57 +0200

    making new changes

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,16 +5,17 @@ import MySQLdb
 from sys import argv
 
 '''
-lists all states with starting name with N
-from the database hbtn_0e_0_usa
+script that lists all states from the database
 '''
 if __name__ == "__main__":
-    con = MySQLdb.connect(
+    cont = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
         password=argv[2], database=argv[3])
-    cursor = con.cursor()
+    cursor = cont.cursor()
     cursor.execute(
-            "SELECT * FROM states WHERE name LIKE BINARY 'N%'ORDER BY id ASC")
+        "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+        (argv[4],)
+        )
     db = cursor.fetchall()
     for i in db:
         print(i)
