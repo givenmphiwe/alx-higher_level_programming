1f7aa41e401c8ca9a65c93c84aa7b2a3cb7fa2fc
givenmphiwe <giggslive52@gmail.com>
2023-04-03 14:55:43 -0700

    advace

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""Fetch
-https://intranet.hbtn.io/status
-using urlib packages
 """
+Python script that takes 2 arguments in order to solve this challenge.
+Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
+from sys import argv
+import requests
 
-import urllib.request
 
-if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
-        response = request.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode('UTF-8')))
-        
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    req = requests.get(url)
+    commits = req.json()
+
+    try:
+        for index in range(10):
+            print("{}: {}".format(
+                commits[index].get("sha"),
+                commits[index].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
