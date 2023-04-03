8f69ef0f55783497adbc9e6ff9dcb1f79d29484f
givenmphiwe <giggslive52@gmail.com>
2023-04-03 15:08:10 -0700

    status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
-
 #!/usr/bin/python3
-""" Uses requests module to fetch URL status"""
-
+"""Script that fetches https://alx-intranet.hbtn.io/status."""
 import requests
 
-if __name__ == '__main__':
-    response = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}"
-          .format(type(response.text), response.text))
-          
+
+if __name__ == "__main__":
+    req = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
