db73b216725d552e0f6d60d6066b266fe9ae97dc
givenmphiwe <giggslive52@gmail.com>
2023-04-03 15:02:46 -0700

    email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL and an email, sends a POST
-request to the passed URL with the email as a parameter, and
-displays the body of the response (decoded in utf-8)
+Script that takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body of the response
+(decoded in utf-8).
 """
+from sys import argv
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
 
-import urllib.request
-import urllib.parse
-import sys
 
-if __name__ == '__main__':
-    data = urllib.parse.urlencode({'email': sys.argv[2]}).encode('ascii')
-    req = urllib.request.Request(sys.argv[1], data)
-    with urllib.request.urlopen(req) as r:
-        html = r.read()
-        print('{}'.format(html.decode('utf-8')))
-        
+if __name__ == "__main__":
+    url = argv[1]
+    value = {"email": argv[2]}
+    data = urlencode(value).encode("ascii")
+    req = Request(url, data)
+
+    with urlopen(req) as response:
+        print(response.read().decode("utf-8", "replace"))
